b0c1de6dbaa89fbeadb0ba69c4cb48b446dc497a
Lawrence Guintu <Lawrence@Lawrences-MacBook-Pro-3.local>
2017-03-16 21:02:08 -0700

    Final Submit

--- public/js/Index.js
+++ public/js/Index.js
@@ -1,2 +1,66 @@
-function displayCardGrid(a){const b=document.createElement("div");b.className="col-sm-3";const c=document.createElement("button");c.className="btn btn-link",c.setAttribute("data-target","#card_detail_modal"),c.setAttribute("data-toggle","modal"),b.appendChild(c);const d=document.createElement("img");d.className="img-responsive";var e=a.storageURL,f=a.name;d.setAttribute("src",e),d.setAttribute("alt",f),c.appendChild(d),galleryRow.appendChild(b)}function displaySearchCardGrid(a){const b=document.createElement("div");b.className="col-sm-3";const c=document.createElement("button");c.className="btn btn-link",b.appendChild(c);const d=document.createElement("img");d.className="img-responsive";var e=a.storageURL,f=a.name;d.setAttribute("attack",a.attack),d.setAttribute("defense",a.defense),d.setAttribute("description",a.description),d.setAttribute("name",f),d.setAttribute("src",e),d.setAttribute("alt",f),addCardListener(d),c.appendChild(d),searchRow.appendChild(b)}
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAhFzSCuiSRmc7VuRehBgtJqvHCow643Gc",
+    authDomain: "yugiohdex-74469.firebaseapp.com",
+    databaseURL: "https://yugiohdex-74469.firebaseio.com",
+    storageBucket: "yugiohdex-74469.appspot.com",
+    messagingSenderId: "928690224323"
+};
+firebase.initializeApp(config);
+
+
+/* create the mouseover listener for the modal function  */
+const modalImage = document.getElementById("modalImage");
+const attackValue = document.getElementById("attackValue");
+const defenseValue = document.getElementById("defenseValue");
+const descriptionValue = document.getElementById("descriptionValue");
+const nameValue = document.getElementById("nameValue");
+function setListener(imgInCol) {
+    imgInCol.addEventListener("mouseover", function() {
+        modalImage.setAttribute("src", imgInCol.getAttribute("src"));
+        attackValue.innerHTML = imgInCol.getAttribute("attack");
+        defenseValue.innerHTML = imgInCol.getAttribute("defense");
+        descriptionValue.innerHTML = imgInCol.getAttribute("description");
+        nameValue.innerHTML = imgInCol.getAttribute("name");
+    });
+}
+
+
+//load the initial monster card gallery
+const galleryRow = document.getElementById("cardGallery");
+const dbCardsRef = firebase.database().ref().child('cards');
+/* This will load the initial selection of the cards, from the database.
+ * Note that we only have so many in the DB that no limit is currently
+ * placed on the object.
+ * */
+dbCardsRef.on('child_added', function(data) {
+    var cardInfo = data.val();
+    if (cardInfo["isMonster"] == null || cardInfo["isMonster"] == true) {
+        const divForCol = document.createElement("div");
+        divForCol.className = "col-sm-3";
+        const buttonInCol = document.createElement("button");
+        buttonInCol.className = "btn btn-link";
+        buttonInCol.setAttribute("data-target", "#card_detail_modal");
+        buttonInCol.setAttribute("data-toggle", "modal");
+        divForCol.appendChild(buttonInCol);
+        const imgInCol = document.createElement("img");
+        imgInCol.className = "img-responsive";
+        var imgURL = cardInfo["storageURL"];
+        var cardName = cardInfo["name"];
+        imgInCol.setAttribute("name", cardName);
+        imgInCol.setAttribute("attack", cardInfo["attack"]);
+        imgInCol.setAttribute("defense", cardInfo["defense"]);
+        imgInCol.setAttribute("description",cardInfo["description"]);
+        imgInCol.setAttribute("src", imgURL);
+        imgInCol.setAttribute("alt", cardName);
+        setListener(imgInCol);
+        buttonInCol.appendChild(imgInCol);
+        galleryRow.appendChild(divForCol);
+    }
+});
+
+
+
+
+
 
